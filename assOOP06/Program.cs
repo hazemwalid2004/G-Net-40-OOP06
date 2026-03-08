ac4e8ee27d3267c9906d23d3da22ccbfd1a7b8aa
Hazem walid <walidf106@gmail.com>
2026-03-08 14:52:52 +0200

    Q2_Part1

--- assOOP06/Program.cs
+++ assOOP06/Program.cs
@@ -13,6 +13,13 @@
             // Give a real-world example (not from the session) that shows the difference between the two.
             //make a coffee/ i don't care how the coffee made press the button to make a coffee
             #endregion
+            #region Q2_Part1
+            // What is the difference between an abstract class and an interface? Give at least four differences.
+            //abstract class -> has an constractor \ has a fields \ has an implemented method \ can use all access modifiers 
+            // interface - > don't has a constractor \ access modifier in Methods public\ class can implement multiple interfaces\contains only abstract methods
+            // When would you choose one over the other?
+            // interface -> when we make 100% abstract / abstract class -> when we want make partial abstract
+            #endregion
         }
     }
 }
