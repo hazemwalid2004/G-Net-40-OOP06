da9593a8e137258e1f4597ca34bc9b635b002202
Hazem walid <walidf106@gmail.com>
2026-03-08 14:32:25 +0200

    Q1_part1

--- assOOP06/Program.cs
+++ assOOP06/Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            #region Q1_part1
+            // What is abstraction in OOP?
+            //the process of exposing only what the user needs and hiding how it is implemented
+            // How is it different from encapsulation?
+            //abstraction-> what an object does / how fields is protected 
+            // Give a real-world example (not from the session) that shows the difference between the two.
+            //make a coffee/ i don't care how the coffee made press the button to make a coffee
+            #endregion
         }
     }
 }
